b471ba357a4544f8167fc502ade93e5771cc0f9b
Nugrawan <126413672+Nugrawan@users.noreply.github.com>
2023-05-23 08:50:58 +0800

    Initialize repository

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
 
 module.exports = {
   entry: './src/index.js',
@@ -36,6 +38,14 @@ module.exports = {
           from: path.resolve(__dirname, './src/public/'),
           to: path.resolve(__dirname, 'dist/'),
         },
+      ],
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 10,
+          progressive: true,
+        }),
       ],
     }),
   ],
